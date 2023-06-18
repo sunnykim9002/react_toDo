865916164794ad4d7cc0b827123f8619f7c49fbe
sunnykim9002 <122718243+sunnykim9002@users.noreply.github.com>
2023-06-18 15:12:14 +0900

    todo list 업데이트

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "../src/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  // style
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float:"right"
+  }
+
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: 'none'
+    }
+  }
+
+  todoDate = [
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: true
+    }
+
+  ]
+
+
+  render () {
+    return(
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
+            <h1>할일 목록</h1>  
+          </div>
+        </div>
+      {this.todoDate.map((data) => (
+        <div style={this.getStyle()} key={data.id}>
+          <input type="checkbox" defaultChecked="false"/>
+          {data.title}
+          <button style={this.btnStyle}>x</button>
+        </div>      
+      ))}
+      </div>
+    )
+  }
 }
-
-export default App;
